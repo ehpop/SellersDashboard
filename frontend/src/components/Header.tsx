0858388e80e7d6342180d3addfec352ec48a132a
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2023-12-08 01:13:43 +0100

    Added simple language dictionary and button for changing language on the page

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,20 +1,21 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import React, {useState} from "react";
+import {Dropdown} from "react-bootstrap";
+import {labelNames} from "../dictionary/HeaderDictionary";
 
 function Header() {
     const expand = 'lg';
     const [theme, setTheme] = useState('dark');
+    const [language, setLanguage] = useState<'pl' | 'en'>('en');
 
     return (
         <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3" data-bs-theme={theme}>
             <Container fluid>
-                <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+                <Navbar.Brand href="#">Sales Portal Pro</Navbar.Brand>
                 <Form className="d-flex vertical align-items-center">
                     <Form.Check
                         data-bs-theme={theme}
@@ -25,10 +26,31 @@ function Header() {
                         }}
                     />
                     <Form.Label className={`ms-2 mb-0 ${theme === 'light' ? 'text-dark' : 'text-light'}`}>
-                        {theme === 'light' ? 'Light' : 'Dark'}
+                        {theme === 'light' ? labelNames.lightTheme[language] : labelNames.darkTheme[language]}
                     </Form.Label>
                 </Form>
+                <Form className={`d-flex align-items-center ${theme === 'light' ? 'text-dark' : 'text-light'}`}>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            {labelNames.selectLanguageButtonLabel[language]}
+                        </Dropdown.Toggle>
 
+                        <Dropdown.Menu>
+                            {
+                                Object.keys(labelNames.language).map((key) => (
+                                    <Dropdown.Item
+                                        key={key}
+                                        onClick={() => {
+                                            setLanguage(key as 'pl' | 'en');
+                                        }}
+                                    >
+                                        {labelNames.language[key as 'pl' | 'en']}
+                                    </Dropdown.Item>
+                                ))
+                            }
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Form>
                 <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`}/>
                 <Navbar.Offcanvas
                     id={`offcanvasNavbar-expand-${expand}`}
@@ -43,14 +65,30 @@ function Header() {
                     </Offcanvas.Header>
                     <Offcanvas.Body>
                         <Nav className="justify-content-end flex-grow-1 pe-3">
-                            <Nav.Link href="#action1">Home</Nav.Link>
-                            <Nav.Link href="#orders">Orders</Nav.Link>
-                            <Nav.Link href="#salesQuality">Sales Quality</Nav.Link>
-                            <Nav.Link href="#customerReviews">Customer Reviews</Nav.Link>
-                            <Nav.Link href="#offerRanking">Offer Ranking</Nav.Link>
-                            <Nav.Link href="#salesChart">Sales Chart</Nav.Link>
-                            <Nav.Link href="#salesTips">Sales Tips</Nav.Link>
-                            <Nav.Link href="#action6">Sing in</Nav.Link>
+                            <Nav.Link href="#action1">
+                                {labelNames.home[language]}
+                            </Nav.Link>
+                            <Nav.Link href="#orders">
+                                {labelNames.orders[language]}
+                            </Nav.Link>
+                            <Nav.Link href="#salesQuality">
+                                {labelNames.salesQuality[language]}
+                            </Nav.Link>
+                            <Nav.Link href="#customerReviews">
+                                {labelNames.customerReviews[language]}
+                            </Nav.Link>
+                            <Nav.Link href="#offerRanking">
+                                {labelNames.offerRanking[language]}
+                            </Nav.Link>
+                            <Nav.Link href="#salesChart">
+                                {labelNames.salesChart[language]}
+                            </Nav.Link>
+                            <Nav.Link href="#salesTips">
+                                {labelNames.salesTips[language]}
+                            </Nav.Link>
+                            <Nav.Link href="#action6">
+                                {labelNames.signIn[language]}
+                            </Nav.Link>
                         </Nav>
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
---
+++ frontend/src/dictionary/HeaderDictionary.ts
@@ -0,0 +1,57 @@
+export interface ILabelNames {
+    [key: string]: {
+        pl: string;
+        en: string;
+    };
+}
+
+export const labelNames = {
+    home: {
+        pl: 'Strona Główna',
+        en: 'Home',
+    },
+    signIn: {
+        pl: 'Zaloguj się',
+        en: 'Sign In',
+    },
+    orders: {
+        pl: 'Zamówienia',
+        en: 'Orders',
+    },
+    salesQuality: {
+        pl: 'Jakość Sprzedaży',
+        en: 'Sales Quality',
+    },
+    customerReviews: {
+        pl: 'Opinie kupujących',
+        en: 'Customer Reviews',
+    },
+    offerRanking: {
+        pl: 'Ranking Ofert',
+        en: 'Offer Ranking',
+    },
+    salesChart: {
+        pl: 'Wykres sprzedaży',
+        en: 'Sales Chart',
+    },
+    salesTips: {
+        pl: 'Porady sprzedażowe',
+        en: 'Sales Tips',
+    },
+    selectLanguageButtonLabel: {
+        pl: 'Wybierz język',
+        en: 'Select language',
+    },
+    language: {
+        pl: 'Polski',
+        en: 'English',
+    },
+    lightTheme: {
+        pl: 'Jasny motyw',
+        en: 'Light theme',
+    },
+    darkTheme: {
+        pl: 'Ciemny motyw',
+        en: 'Dark theme',
+    }
+};
