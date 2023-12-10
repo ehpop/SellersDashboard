a452c0c53ec15acc95b8a25613799ba6190d85c4
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2023-12-10 15:56:52 +0100

    created router for application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function App({children}: any) {
     return (
         <div className="App">
-          <Header/>
+            <Header/>
+            {children}
+            <Footer/>
         </div>
     );
+}
+
+export function appLoader() {
+    return <></>;
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter, Outlet,
+    RouterProvider
+} from "react-router-dom";
+import App, {appLoader} from './App';
+import Orders, {ordersLoader} from "./pages/Orders";
+import SalesQuality, {salesQualityLoader} from "./pages/SalesQuality";
+import CustomerReviews, {customerReviewsLoader} from "./pages/CustomerReviews";
+import Login, {loginLoader} from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Home, {homeLoader} from "./pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App><Outlet/></App>,
+        loader: appLoader,
+        children: [
+            {
+                path: "home",
+                element: <Home/>,
+                loader: homeLoader
+            },
+            {
+                path: "orders",
+                element: <Orders/>,
+                loader: ordersLoader,
+            },
+            {
+                path: "salesQuality",
+                element: <SalesQuality/>,
+                loader: salesQualityLoader,
+            },
+            {
+                path: "customerReviews",
+                element: <CustomerReviews/>,
+                loader: customerReviewsLoader,
+            },
+            {
+                path: "login",
+                element: <Login/>,
+                loader: loginLoader,
+            },
+            {
+                path: "*",
+                element: <NotFound/>,
+            }
+        ],
+    }
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
