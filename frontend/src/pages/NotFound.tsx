ab143cc5bef23b1e4d369c2966a957eb0169cc63
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2023-12-12 19:32:40 +0100

    Added translation to NotFound.tsx

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles/NotFound.css";
 import useDocumentTitle from "../hooks/useDocumentTitle";
+import {FormattedMessage} from "react-intl";
 
 function NotFound() {
     useDocumentTitle("Not found");
@@ -8,9 +9,12 @@ function NotFound() {
     return (
         <div className="main-div">
             <h1 className="error-code">404</h1>
-            <h2 className="error-reason">Not found</h2>
-            <h3 className="error-explanation">Resource could not be found</h3>
-            <a className="return-home" href={"/home"}>Return to home page</a>
+            <h2 className="error-reason"><FormattedMessage id={"not-found.error-reason"} defaultMessage={"Not Found"}/>
+            </h2>
+            <h3 className="error-explanation"><FormattedMessage id={"not-found.error-explanation"}
+                                                                defaultMessage={"Resource could not be found"}/></h3>
+            <a className="return-home" href={"/home"}><FormattedMessage id={"not-found.return-home"}
+                                                                        defaultMessage={"Return to home page"}/></a>
         </div>
     );
 }
