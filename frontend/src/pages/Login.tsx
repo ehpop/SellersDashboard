eaa30013c466e01dc21b80400093a32f8e393d03
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-01-20 21:51:54 +0100

    added dummy login component

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, {useContext} from "react";
 import Form from "react-bootstrap/Form";
 import {Button} from "react-bootstrap";
 import './styles/Login.css';
 import useDocumentTitle from "../hooks/useDocumentTitle";
 import Container from "react-bootstrap/Container";
 import {FormattedMessage, useIntl} from "react-intl";
+import AuthContext from "../context/AuthContext";
+import {useNavigate} from "react-router-dom";
 
 function Login() {
     useDocumentTitle("Login");
-    const intl = useIntl();
+    const navigate = useNavigate();
+    const [email, setEmail] = React.useState<string>("");
+    const [password, setPassword] = React.useState<string>("");
 
-    const handleSubmit = (event: React.MouseEvent) => {
+    const intl = useIntl();
+    const authContext = useContext(AuthContext);
+
+
+    const handleSubmit = (event: any) => {
         event.preventDefault();
-        console.log(event);
+
+        console.log(email, password);
+
+        if (email === "stud@pw.ed.pl" && password === "zetis") {
+            authContext.setAuth(true);
+        } else if (email === "stud2@pw.edu.pl" && password === "zetis") {
+            authContext.setAuth(true);
+        } else if (email === "stud3@pw.edu.pl" && password === "zetis") {
+            authContext.setAuth(true);
+        } else {
+            alert("Wrong credentials");
+            return;
+        }
+
+        alert("Logged in");
+        navigate("/");
     }
 
     return(
@@ -41,7 +64,9 @@ function Login() {
                                 id: "login.emailPlaceholder",
                                 defaultMessage: "Enter email"
                             }
-                        )}/>
+                        )} onChange={(event) => {
+                            setEmail(event.target.value)
+                        }}/>
                         <Form.Text className="text-muted">
                             <FormattedMessage id={"login.emailHelp"}
                                               defaultMessage={"We'll never share your email with anyone else."}/>
@@ -55,7 +80,9 @@ function Login() {
                         <Form.Control type="password" placeholder={intl.formatMessage({
                             id: "login.passwordPlaceholder",
                             defaultMessage: "Password"
-                        })}/>
+                        })} onChange={(event) => {
+                            setPassword(event.target.value)
+                        }}/>
                     </Form.Group>
                     <Button variant="primary" type="submit" onClick={handleSubmit}>
                         <FormattedMessage id={"login.submitButtonLabel"} defaultMessage={"Submit"}/>
