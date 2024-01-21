783a16625aadd0ec509c5bebb25d3d7cf6f19ca9
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-01-22 00:29:22 +0100

    refactored ProtectedRoute.tsx to be translated

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,14 +1,18 @@
 import {useContext} from "react";
 import AuthContext from "../context/AuthContext";
 import {Link} from "react-router-dom";
+import {FormattedMessage} from "react-intl";
 
 const ProtectedRoute = ({children}: any) => {
     const authContext = useContext(AuthContext);
 
     const unauthenticated = (
-        <div className="d-flex justify-content-center align-items-center h-100">
+        <div className="d-flex justify-content-center align-items-center vh-100">
             <h1 className="text-center">
-                Sorry to access this page you need to <Link to={"/login"}>log in</Link>
+                <FormattedMessage id={"unauthenticated"} defaultMessage={"Sorry to access this page you need to "}/>
+                <Link to={"/login"}>
+                    <FormattedMessage id={"unauthenticated.login"} defaultMessage={"log in"}/>
+                </Link>
             </h1>
         </div>
     );
